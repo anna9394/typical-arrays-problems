b8ca714234b83d8937dccdcaddeb3bdc7306e336
Anna <annaandreevna90394@gmail.com>
2021-02-09 13:23:59 +0300

    feat: add functions implementations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (max < array[i]) {
+            max = array[i];
+        }
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length === 0) {
+        return 0;
+    }
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+    return result / array.length;
+};
